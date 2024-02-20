0a00fc44523f588b5b07ca196c7f938c3588d0b1
Ayushi-Rahane <ayushirahane2021@gmail.com>
2024-02-20 15:55:54 +0530

    C

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
   <link rel="shortcut icon" type="x-icon" href="cnc-logo.png">
+  <title>Concept N Controls</title>
 
   <link rel="stylesheet" href="style/navigation.css">
   <link rel="stylesheet" href="style/about-us.css">
